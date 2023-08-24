2b296dc55d2e0c5b41ece53d8bbd49fe8c83eb1c
hoonsbory <tonymkcv@naver.com>
2023-08-25 01:18:37 +0900

    [chore] 프로젝트 루트 path 설정

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/ranking?genre=romance',
+        basePath: false,
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
