6392a3f8670ca404af455347f234a664385cabae
hoonsbory <tonymkcv@naver.com>
2023-08-25 01:55:10 +0900

    [style&feat] react query wrapping 및 css normalize

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,32 @@
-import '@styles/globals.css';
+import { Global, css } from '@emotion/react';
+import { globalCss } from '@styles/globals';
+import normalize from 'emotion-normalize';
 import type { AppProps } from 'next/app';
+import { useState } from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+          },
+        },
+      }),
+  );
+  return (
+    <>
+      <Global
+        styles={css`
+          ${normalize}
+          ${globalCss}
+        `}
+      />
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
+  );
 }
