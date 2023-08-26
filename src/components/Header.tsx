498e21bb0a0e7bdafaa4b7d98ac47ad41e58b4a4
hoonsbory <tonymkcv@naver.com>
2023-08-26 19:34:22 +0900

    [feat] 스타일링 완료 및 네비게이션 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,39 @@
-import React from 'react';
+import { css } from '@emotion/react';
+import { genreType } from '@pages/ranking';
+import { defaultBtnCss } from '@styles/commonStyles';
+import Link from 'next/link';
 
-const Header = () => {
+const genreKo = {
+  romance: '로맨스',
+  drama: '드라마',
+};
+
+const headerCss = css`
+  display: grid;
+  grid-template-columns: 60% 40%;
+  align-items: center;
+  a {
+    margin-right: 5px;
+    ${defaultBtnCss}
+    color: white;
+    background-color: var(--defaultButtonSelectedColor);
+    padding: 5px 10px;
+    @media (hover: hover) {
+      :hover {
+        background-color: #eb0026ad;
+      }
+    }
+  }
+`;
+
+const Header = ({ genre }: { genre: genreType }) => {
   return (
-    <header>
-      <h1>로맨스 장르 랭킹</h1>
+    <header css={headerCss}>
+      <h1>{genreKo[genre]} 장르 랭킹</h1>
+      <nav>
+        <Link href={'/ranking?genre=romance'}>로맨스</Link>
+        <Link href={'/ranking?genre=drama'}>드라마</Link>
+      </nav>
     </header>
   );
 };
