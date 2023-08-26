25dcde5b1f471809ced99fb95fb502b5e9393659
hoonsbory <tonymkcv@naver.com>
2023-08-27 04:32:13 +0900

    [intasll&init] 테스트를 위한 라이브러리 설치 및 jest config 설정

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  moduleNameMapper: {
+    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@pages/(.*)$': '<rootDir>/src/pages/$1',
+    '^@styles/(.*)$': '<rootDir>/src/styles/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+};
+
+module.exports = createJestConfig(customJestConfig);
