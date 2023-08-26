f7221c85bcf60e5553b80339ca4b6cc6e3057a3d
hoonsbory <tonymkcv@naver.com>
2023-08-27 04:30:28 +0900

    [test] 총 6가지 렌더링 테스트 추가 무한스크롤 렌더링 테스트 첫 페이지 렌더링 테스트 연재중 필터와 완결 필터 중복 테스트 무료회차 3회 이상 필터 테스트 단행본 필터 테스트

---
+++ src/test/Comic.spec.tsx
@@ -0,0 +1,112 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { act, fireEvent, render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import ComicsContainer from '@components/ComicsContainer';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import dramaComicDatas from '@pages/datas/drama/dramaComicDatas';
+import { ReactNode } from 'react';
+
+let observerCallback: IntersectionObserverCallback;
+
+const server = setupServer(
+  rest.get('/api/comics/drama', (req, res, ctx) => {
+    const page = req.url.searchParams.get('page');
+    return res(ctx.json(dramaComicDatas[Number(page) - 1]));
+  }),
+);
+
+beforeAll(() => {
+  server.listen();
+});
+
+beforeEach(() => {
+  global.IntersectionObserver = jest.fn(callback => {
+    observerCallback = callback;
+
+    return {
+      observe: jest.fn(),
+      disconnect: jest.fn(),
+      unobserve: jest.fn(),
+      takeRecords: jest.fn(),
+    };
+  });
+});
+
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
+
+const wrapper = ({ children }: { children: ReactNode }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+);
+
+const simulateScrollEvent = async (times: number) => {
+  for (let i = 0; i < times; i++) {
+    await act(async () => observerCallback([{ isIntersecting: true }]));
+  }
+};
+
+export default wrapper;
+
+describe('메인페이지 테스트', () => {
+  test('스크롤이 끝까지 내려가면 다음페이지를 렌더링한다.', async () => {
+    await act(async () => render(<ComicsContainer genre="drama" />, { wrapper }));
+    expect(global.IntersectionObserver).toHaveBeenCalled();
+    await simulateScrollEvent(4);
+    const newContentElement = screen.getByText(/데스크레센도/i);
+    expect(newContentElement).toBeInTheDocument();
+  });
+
+  test('페이지가 처음 로딩되면 1페이지를 렌더링한다', async () => {
+    await act(async () => render(<ComicsContainer genre="drama" />, { wrapper }));
+    const newContentElement = screen.getByText(/시크릿 동맹/i);
+    expect(newContentElement).toBeInTheDocument();
+  });
+
+  test('연재중 필터가 클릭되면 완결 웹툰은 렌더링 되지 않는다', async () => {
+    await act(async () => render(<ComicsContainer genre="drama" />, { wrapper }));
+    const button = screen.getByText('연재중');
+    fireEvent.click(button);
+    const completedElements = screen.getAllByText(/완결/i).filter(i => i.tagName !== 'BUTTON');
+    expect(completedElements.length).toBe(0);
+  });
+
+  test('완결 필터가 클릭되면 연재중 웹툰은 렌더링 되지 않는다', async () => {
+    await act(async () => render(<ComicsContainer genre="drama" />, { wrapper }));
+    const button = screen.getAllByText('완결').find(i => i.tagName === 'BUTTON');
+    fireEvent.click(button!);
+    const completedElements = screen.getAllByText(/연재/i).filter(i => i.tagName !== 'BUTTON');
+    expect(completedElements.length).toBe(0);
+  });
+
+  test('무료회차 3회 필터가 클릭되면 무료회차가 3회 이상인 웹툰만 렌더링된다', async () => {
+    await act(async () => render(<ComicsContainer genre="drama" />, { wrapper }));
+    const button = screen.getByText(/무료회차/);
+    fireEvent.click(button!);
+    const completedElements = screen
+      .getAllByText(/무료/i)
+      .filter(i => i.tagName !== 'BUTTON')
+      .map(i => i.textContent?.split(' ')[0].split('화')[0]);
+    completedElements.forEach(i => expect(Number(i) >= 3).toBeTruthy());
+  });
+
+  test('단행본 필터가 클릭되면 단행본인 웹툰만 렌더링된다', async () => {
+    await act(async () => render(<ComicsContainer genre="drama" />, { wrapper }));
+    const button = screen.getByText(/단행본/);
+    const shortComics = dramaComicDatas[0].data.comicRankList.filter(i => i.isPrint);
+    fireEvent.click(button!);
+    for (const comic of shortComics) {
+      const elem = await screen.findByTestId(comic.id);
+      expect(elem.textContent).toBe(comic.title);
+    }
+  });
+});
